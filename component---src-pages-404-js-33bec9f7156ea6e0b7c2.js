31c8ac6af4c0093502085cee9a147e80a22dede6
luisgepeto <luisgepeto@users.noreply.github.com>
2021-10-05 20:15:13 +0000

    Deploying to gh-pages from @ luisgepeto/SuperSuper@f33f3e8f6ea623368b6b96902fdf599fa90f88ee 🚀

---
+++ component---src-pages-404-js-33bec9f7156ea6e0b7c2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("CyKR"),o=n("ohv3");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-33bec9f7156ea6e0b7c2.js.map
